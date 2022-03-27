e3378b69cfafd0c93c521d58a73cccad2ce82abf
Sergey <popovserhio@gmail.com>
2022-03-27 22:48:51 +0300

    1

--- Homework_21.03.22.py
+++ Homework_21.03.22.py
@@ -1,6 +1,14 @@
-input_case = input("Выбери тип операции:\n1 +\n2 -\n3 *\n4 /")
-value_1 = input("Введи первое число:")
-value_2 = input("Введи второе число:")
+input_case = input("Выбери тип операции:\n1 +\n2 -\n3 *\n4 /\n")
+
+value_1 = float(input("Введи первое число:"))
+value_2 = float(input("Введи второе число:"))
 if input_case == '1':
-    result = 'value_1' + 'value_2'
-print(result)
+    print(value_1 + value_2)
+elif input_case == '2':
+    print(value_1 - value_2)
+elif input_case == '3':
+    print(value_1 * value_2)
+elif input_case == '4':
+    print(value_1 / value_2)
+else:
+    print("Такой операции нет!!!")
