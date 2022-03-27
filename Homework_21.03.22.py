2aeb8e91889585ea1998c7f7711c951289ffec59
Sergey <popovserhio@gmail.com>
2022-03-27 21:06:53 +0300

    1

--- Homework_21.03.22.py
+++ Homework_21.03.22.py
@@ -1,1 +1,6 @@
-print('hello!!!') # fg
+input_case = input("Выбери тип операции:\n1 +\n2 -\n3 *\n4 /")
+value_1 = input("Введи первое число:")
+value_2 = input("Введи второе число:")
+if input_case == '1':
+    result = 'value_1' + 'value_2'
+print(result)
