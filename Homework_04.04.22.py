725a5fa23ab9edc414baacd1338651b87a877818
Sergey <popovserhio@gmail.com>
2022-04-11 10:36:15 +0300

    1

--- Homework_04.04.22.py
+++ Homework_04.04.22.py
@@ -1,23 +1,23 @@
 # 1. Дано целое число (int). Определить сколько нулей в этом числе.
 
-# my_int = 25470005100
-# symbol = str(0)
-# my_str = str(my_int)
-# my_result = my_str.count(symbol)
-# print(my_result)
+my_int = 25470005100
+symbol = str(0)
+my_str = str(my_int)
+my_result = my_str.count(symbol)
+print(my_result)
 
 
 
 
 # 2. Дано целое число (int). Определить сколько нулей в конце этого числа. Например для числа 1002000 - три нуля
-# my_int = 100200
-# len_1 = len(str(my_int))
-#
-# my_int_reverse = int(str(my_int)[::-1])
-# len_2 = len(str(my_int_reverse))
-#
-# result = len_1 - len_2
-# print(result)
+my_int = 100200
+len_1 = len(str(my_int))
+
+my_int_reverse = int(str(my_int)[::-1])
+len_2 = len(str(my_int_reverse))
+
+result = len_1 - len_2
+print(result)
 
 
 
@@ -28,18 +28,18 @@
 # Создать список my_result в который вначале поместить
 # элементы на четных местах из my_list_1, а потом все элементы на нечетных местах из my_list_2.
 
-# my_list_1 = [1, 2, 3, 4, 5, ]
-# my_srez_1 = my_list_1[0::2]
-#
-# my_list_2 = [10, 20, 30, 40, 50, ]
-# my_srez_2 = my_list_2[1::2]
-#
-# my_result = []
-# print(id(my_result))
-# my_result.extend(my_srez_1)
-# my_result.extend(my_srez_2)
-#
-# print(my_result, id(my_result))
+my_list_1 = [1, 2, 3, 4, 5, ]
+my_srez_1 = my_list_1[0::2]
+
+my_list_2 = [10, 20, 30, 40, 50, ]
+my_srez_2 = my_list_2[1::2]
+
+my_result = []
+print(id(my_result))
+my_result.extend(my_srez_1)
+my_result.extend(my_srez_2)
+
+print(my_result, id(my_result))
 
 
 
@@ -48,11 +48,11 @@
 # 4. Дан список my_list. СОЗДАТЬ НОВЫЙ список new_list у которого первый элемент из my_list
 # стоит на последнем месте. Если my_list [1,2,3,4], то new_list [2,3,4,1]
 
-# my_list = [1, 22, 33, ]
-# symbol = my_list[0]
-# new_list = my_list[1::]
-# new_list.append(symbol)
-# print(new_list)
+my_list = [1, 22, 33, ]
+symbol = my_list[0]
+new_list = my_list[1::]
+new_list.append(symbol)
+print(new_list)
 
 
 
@@ -61,11 +61,11 @@
 # 5.Дан список my_list. В ЭТОМ списке первый элемент переставить на последнее место.
 # [1,2,3,4] -> [2,3,4,1]. Пересоздавать список нельзя! (используйте метод pop)
 
-# my_list = [1, 2, 3, 4]
-# symbol = my_list.pop(0)
-# my_list.append(symbol)
-#
-# print(my_list)
+my_list = [1, 2, 3, 4]
+symbol = my_list.pop(0)
+my_list.append(symbol)
+
+print(my_list)
 
 
 
@@ -76,10 +76,10 @@
 # Для данного примера ответ - 133. (используйте split и проверку isdigit)
 
 
-# my_str = "6 больше чем 2 но меньше чем 8"
-# numbers = [int(numbers) for numbers in str.split(my_str) if numbers.isdigit()]
-# result = sum(numbers)
-# print(result)
+my_str = "6 больше чем 2 но меньше чем 8"
+numbers = [int(numbers) for numbers in str.split(my_str) if numbers.isdigit()]
+result = sum(numbers)
+print(result)
 
 
 
@@ -90,13 +90,13 @@
 # В переменную sub_str поместить НАИБОЛЬШУЮ часть строки между этими символами.
 # my_str = "My long string", l_limit = "o", r_limit = "g" -> sub_str = "ng strin".
 
-# my_str = "level up or down"
-# l_limit = "l"
-# r_limit = "o"
-# result_1 = my_str.find(l_limit) + 1
-# result_2 = my_str.rfind(r_limit)
-# sub_str = my_str[result_1:result_2]
-# print(sub_str)
+my_str = "level up or down"
+l_limit = "l"
+r_limit = "o"
+result_1 = my_str.find(l_limit) + 1
+result_2 = my_str.rfind(r_limit)
+sub_str = my_str[result_1:result_2]
+print(sub_str)
 
 
 
@@ -106,16 +106,16 @@
 # быть заменен подчеркиванием ('_'). Примеры: 'abcd' -> ['ab', 'cd'], 'abcde' -> ['ab', 'cd', e_']
 # (используйте срезы длинны 2)
 
-# text = '43644213'
-# para = 2
-# symbol = '_'
-# if len(text) % 2 == 0:
-#     result = [text[i:i + para] for i in range(0, len(text), para)]
-#
-# else:
-#     text += symbol
-#     result = [text[i:i + para] for i in range(0, len(text), para)]
-# print(result)
+text = '43644213'
+para = 2
+symbol = '_'
+if len(text) % 2 == 0:
+    result = [text[i:i + para] for i in range(0, len(text), para)]
+
+else:
+    text += symbol
+    result = [text[i:i + para] for i in range(0, len(text), para)]
+print(result)
 
 
 
@@ -126,13 +126,13 @@
 # Крайние элементы списка никогда не учитываются, поскольку у них недостаточно соседей.
 # Для списка [2,4,1,5,3,9,0,7] ответом будет 3 потому что 4 > 2+1, 5 > 1+3, 9>3+0.
 
-# my_list = [2, 4, 1, 5, 3, 9, 0, 7]
-# result = 0
-# for num in range(1, len(my_list) - 1):
-#     if int(my_list[num - 1]) < int(my_list[num]) and int(my_list[num]) > int(my_list[num + 1]):
-#         result += 1
-# print(result)
-#
+my_list = [2, 4, 1, 5, 3, 9, 0, 7]
+result = 0
+for num in range(1, len(my_list) - 1):
+    if int(my_list[num - 1]) < int(my_list[num]) and int(my_list[num]) > int(my_list[num + 1]):
+        result += 1
+print(result)
+
 
 
 
@@ -142,25 +142,25 @@
 # Создать новый список в который поместить только строки из my_list.
 
 
-# my_list = [1, 2, 3, "11", "22", 33]
-#
-# new_list = []
-# for symbol in my_list:
-#     if type(symbol) == str:
-#         new_list.append(symbol)
-# print(new_list)
+my_list = [1, 2, 3, "11", "22", 33]
+
+new_list = []
+for symbol in my_list:
+    if type(symbol) == str:
+        new_list.append(symbol)
+print(new_list)
 
 
 # 11. Дана строка my_str. Создать список в который поместить те символы из my_str,
 # которые встречаются в строке ТОЛЬКО ОДИН раз.
 
-# my_str = 'aaaaaaavvvvvvvvvnnnnnnkm1'
-# my_list = []
-#
-# for symbol in my_str:
-#     if my_str.count(symbol) == 1:
-#         my_list.append(symbol)
-# print(my_list)
+my_str = 'aaaaaaavvvvvvvvvnnnnnnkm1'
+my_list = []
+
+for symbol in set(my_str):
+    if my_str.count(symbol) == 1:
+        my_list.append(symbol)
+print(my_list)
 
 
 
@@ -168,13 +168,14 @@
 # которые есть в обеих строках хотя бы раз.
 
 
-# my_str_1 = "samsa  ujhduhfi"
-# my_str_2 = "samsa"
-# my_list = []
-# for item in my_str_1:
-#     if item in my_str_2:
-#         my_list.append(item)
-# print(my_list)
+my_str_1 = "samsa s  ujhduhfi"
+my_str_2 = "samsa"
+my_list = []
+result = set(my_str_1) & set(my_str_2)
+
+for symbol in result :
+    my_list.append(symbol)
+print(my_list)
 
 
 
@@ -183,11 +184,14 @@
 # но в каждой ТОЛЬКО ПО ОДНОМУ разу.
 # Пример: для строк "aaaasdf1" и "asdfff2" ответ ["s", "d"], т.к. эти символы есть в каждой строке по одному разу
 
-# my_str_1 = "aaaasdf1"
-# my_str_2 = "asdfff2"
-# my_list = []
-# for item in my_str_1:
-#     if item in my_str_2:
-#         if my_str_2.count(item) == 1 and my_str_1.count(item) == 1:
-#             my_list.append(item)
-# print(my_list)
+my_str_1 = "aaaasdf1"
+my_str_2 = "asdfff2"
+my_list = []
+result = set(my_str_1) & set(my_str_2)
+
+for symbol in result :
+    if my_str_2.count(symbol) == 1 and my_str_1.count(symbol) == 1:
+        my_list.append(symbol)
+print(my_list)
+
+
